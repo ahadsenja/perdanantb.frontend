4ff03cfbbc04ef6484b88fa07a10cd5cca5f553e
ahadsenja <hamdanilm96@gmail.com>
2020-01-06 23:21:33 +0800

    chore: update member list data

--- src/app/Member/MemberList.js
+++ src/app/Member/MemberList.js
@@ -20,13 +20,51 @@ class ProfileMember extends React.Component {
                                         <Col>
                                             <Container className="themed-container">
                                                 <h3>{member.full_name}</h3>
-                                                <br /> 
-                                                <strong>Agama : </strong> {member.religion} 
-                                                <br /> <br />
-                                                <strong>Jenis Kelamin : </strong> {member.gender} 
-                                                <br /> <br />
-                                                <strong>Tempat/Tgl Lahir : </strong> {member.born_place}, {member.born_date} <br /> <br />
-                                                <strong>Klub : </strong> {member.club.name}
+                                                <br />
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Jenis Kelamin</strong>
+                                                    <p><strong>:</strong> {member.gender}</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Tempat Lahir</strong>
+                                                    <p><strong>:</strong> {member.born_place}</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Tanggal Lahir</strong>
+                                                    <p><strong>:</strong> {member.born_date}</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>No.Hp</strong>
+                                                    <p><strong>:</strong> {member.phone}</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Klub</strong>
+                                                    <p><strong>:</strong> {member.club.name}</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Satuan</strong>
+                                                    <p><strong>:</strong> {member.satuan.name}</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Tinggi Badan</strong>
+                                                    <p><strong>:</strong> {member.body_height} cm</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Berat Badan</strong>
+                                                    <p><strong>:</strong> {member.body_weight} kg</p>
+                                                </div>
+
+                                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                    <strong>Panjang Tarikan</strong>
+                                                    <p><strong>:</strong> {member.draw_length} inch</p>
+                                                </div>
                                             </Container>
                                         </Col>
 
@@ -35,46 +73,6 @@ class ProfileMember extends React.Component {
                                                 <Button color='primary' onClick={this.props.toggleMemberForm}>Edit</Button>
                                             </div>
                                         </Col>
-                                    </Row>
-
-                                    {/* Body Information */}
-                                    <br /> <br /> <br />
-                                    <Row>
-                                        <Col>
-                                            <strong>Tinggi Badan</strong> <br />
-                                            {member.body_height} Cm
-                                        </Col>
-                                        <Col>
-                                            <strong>Berat Badan</strong> <br />
-                                            {member.body_weight} Kg
-                                        </Col>
-                                        <Col>
-                                            <strong>Panjang Tarikan</strong> <br />
-                                            {member.draw_length} Inch
-                                        </Col>
-                                    </Row>
-
-                                    <hr />
-                                    {/* Detail Information */}
-                                    <br />
-                                    <Row>
-                                        <Col md='3'><strong>Agama</strong></Col>
-                                        <Col> : {member.religion} </Col>
-                                    </Row>
-                                    <br />
-                                    <Row>
-                                        <Col md='3'><strong>Jenis Kelamin</strong></Col>
-                                        <Col> : {member.gender} </Col>
-                                    </Row>
-                                    <br />
-                                    <Row>
-                                        <Col md='3'><strong>Tempat Lahir</strong></Col>
-                                        <Col> : {member.born_place} </Col>
-                                    </Row>
-                                    <br />
-                                    <Row>
-                                        <Col md='3'><strong>Tanggal Lahir</strong></Col>
-                                        <Col> : {member.born_date} </Col>
                                     </Row>
                                 </div>
                             )
